a07e76998e7c333c22b452759daf68a3c760dc1e
Maartje Oostvogels <moostvogels@hotmail.com>
2022-12-31 16:14:37 +0000

    Added basic code to index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,47 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What was your motivation to build this project?',
+        name: 'motivation',
+      },
+//      {
+//          type: 'input',
+//          message: 'What problem does it solve?',
+//          name: 'problemsolve',
+//      },
+//      {
+//          type: 'input',
+//          message: 'What did you learn?',
+//          name: 'username',
+//      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("File written successfully\n");
+          console.log("The file has the following content:");
+          console.log(fs.readFileSync(fileName, "utf8"));
+        }
+      });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile("README.md", answers)
+      });
+}
 
 // Function call to initialize app
 init();
