f1083339d9f5576db9c4961a2b7602d90e8bab16
Maartje Oostvogels <moostvogels@hotmail.com>
2022-12-31 17:59:36 +0000

    Added questions and badge functionality

--- Develop/index.js
+++ Develop/index.js
@@ -4,43 +4,83 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
+/*
 const questions = [
-    {
-        type: 'input',
-        message: 'What was your motivation to build this project?',
-        name: 'motivation',
-      },
-//      {
-//          type: 'input',
-//          message: 'What problem does it solve?',
-//          name: 'problemsolve',
-//      },
-//      {
-//          type: 'input',
-//          message: 'What did you learn?',
-//          name: 'username',
-//      },
+  {
+    type: 'input',
+    message: 'Provide a short description explaining the what, why, and how of your project.',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'hat problem does it solve?',
+    name: 'table of contents',
+  },
+  {
+    type: 'input',
+    message: 'What are the steps required to install your project?',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Provide instructions and examples for use.',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'List details of any creators, followed tutorials and collaborator names with GitHub profiles here.',
+    name: 'credits',
+  },
+  {
+    type: 'list',
+    message: 'What license would you like to add to your README file?',
+    name: 'license',
+    choices: ['Apache License 2.0','GNU GPLv3', 'MIT', 'ISC'],
+  },
+  {
+    type: 'input',
+    message: 'If you would like other developers to contribute to your application or package, please include guidelines for how to do so.',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'Write tests for your application and provide examples on how to run them here.',
+    name: 'tests',
+  },
+];
+*/
+
+const questions = [
+  {
+    type: 'list',
+    message: 'What license would you like to add to your README file?',
+    name: 'license',
+    choices: ['Apache License 2.0','GNU GPLv3', 'MIT', 'ISC', ],
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateMarkdown(data), (err) => {
-        if (err)
-          console.log(err);
-        else {
-          console.log("File written successfully\n");
-          console.log("The file has the following content:");
-          console.log(fs.readFileSync(fileName, "utf8"));
-        }
-      });
-}
+  console.log(data);
+  const contents = generateMarkdown(data);
+  console.log('Writing the following to file...', contents);
 
+  fs.writeFile(fileName, contents, (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log("The file has the following content:");
+      console.log(fs.readFileSync(fileName, "utf8"));
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-        writeToFile("README.md", answers)
-      });
+  inquirer.prompt(questions).then((answers) => {
+    writeToFile("README.md", answers);
+  });
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badgeUrl;
+  switch (license) {
+    case 'Apache License 2.0':
+      badgeUrl = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
+    case 'GNU GPLv3':
+      badgeUrl = 'https://img.shields.io/badge/License-GPLv3-blue.svg';
+    case 'MIT':
+      badgeUrl = 'https://img.shields.io/badge/license-MIT-blue';
+    case 'ISC':
+      badgeUrl = 'https://img.shields.io/badge/License-ISC-blue.svg';
+    case 'None':
+      badgeUrl = '';
+  }
+
+  return `![License](${badgeUrl})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +28,17 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+  console.log('generateMarkdown: the license is: ', data.license);
+
+  const licenseBadge = renderLicenseBadge(data.license);
+
+  console.log('generateMarkdown: license badge is: ', licenseBadge);
+
+  return `# ${data.title}
+${licenseBadge}
+
+# License
 `;
 }
 
